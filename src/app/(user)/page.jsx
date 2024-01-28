abdda738ee3d7c2cdd59d615f9988fbd1f7a101e
sohseyedi <sohseyedi@gmail.com>
2024-01-28 11:43:41 +0330

    user page

--- src/app/(user)/page.jsx
+++ src/app/(user)/page.jsx
@@ -1,10 +1,7 @@
-"use client"
-import HeaderLayout from "@/components/HeaderLayout";
 
 export default function Home() {
   return (
     <main className="text-3xl">
-      <HeaderLayout/>
       <div>صفحه خانه</div>
     </main>
   );
