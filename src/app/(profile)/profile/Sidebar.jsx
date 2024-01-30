72803eb716538e2ffc10246a1c1a64ec85919e12
sohseyedi <sohseyedi@gmail.com>
2024-01-30 11:41:18 +0330

    profile route

---
+++ src/app/(profile)/profile/Sidebar.jsx
@@ -0,0 +1,30 @@
+"use client";
+import SidebarLayout from "@/components/SidebarLayout";
+import { CustomNavlink } from "@/ui/CustomNavlink";
+import { HiOutlineHome, HiOutlineUser } from "react-icons/hi2";
+import { RiLayoutMasonryLine } from "react-icons/ri";
+
+const Sidebar = () => {
+  return (
+    <SidebarLayout>
+      <CustomNavlink to={"/"}>
+        <HiOutlineHome size={26} />
+        <h6>صفحه اصلی</h6>
+      </CustomNavlink>
+      <CustomNavlink to={"/profile"}>
+        <RiLayoutMasonryLine size={26} />
+        <h6>داشبورد</h6>
+      </CustomNavlink>
+      <CustomNavlink to={"/profile/me"}>
+        <HiOutlineUser size={26} />
+        <h6>اطلاعات کاربری</h6>
+      </CustomNavlink>
+      <CustomNavlink to={"/profile/advertising"}>
+        <RiShoppingBagLine size={26} />
+        <h6>اگهی های من</h6>
+      </CustomNavlink>
+    </SidebarLayout>
+  );
+};
+
+export default Sidebar;
