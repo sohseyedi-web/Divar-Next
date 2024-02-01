c23cb61a1198880cf11242ce2364142c86749785
sohseyedi <sohseyedi@gmail.com>
2024-02-01 21:09:42 +0330

    products header

--- src/app/(user)/products/HeaderProducts.jsx
+++ src/app/(user)/products/HeaderProducts.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+"use client";
+
+import { useGetUser } from "@/hooks/useUser";
+import { SiRobotframework } from "react-icons/si";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import Link from "next/link";
+import { useState } from "react";
+import Modal from "@/ui/Modal";
+import AdvertisingForm from "@/components/AdvertisingForm";
 
 const HeaderProducts = () => {
-  return (
-    <div>HeaderProducts</div>
-  )
-}
+  const { user, isLoading } = useGetUser();
+  const [open, setOpen] = useState(false);
 
-export default HeaderProducts
+  return (
+    <header className="py-3 px-6 container mx-auto flex items-center justify-between shadow border-b border-gray-200">
+      <div className="flex items-center gap-x-4">
+        <div className="flex items-center gap-x-2">
+          <SiRobotframework size={32} />
+          <h4 className="text-2xl font-bold">دیوار</h4>
+        </div>
+        <input
+          type="text"
+          placeholder="جست و جو آگهی"
+          className="input input-bordered w-[350px] focus:bg-white bg-gray-200 text-right transition-all duration-300 outline-none"
+        />
+      </div>
+
+      <div className="flex items-center gap-x-2">
+        {user ? (
+          <>
+            <div className="flex items-center gap-x-1">
+              <HiOutlineLocationMarker size={28} />
+              {user?.city}
+            </div>
+            <Modal
+              onClose={() => setOpen(false)}
+              open={open}
+              title={"ایجاد آگهی"}
+            >
+              <AdvertisingForm onClose={() => setOpen(false)} />
+            </Modal>
+            <button
+              onClick={() => setOpen(true)}
+              className="btn bg-rose-600 text-white w-[120px] border-none text-lg"
+            >
+              ثبت آگهی
+            </button>
+          </>
+        ) : (
+          <Link
+            href={"/auth"}
+            className="btn btn-success text-white w-[150px]  border-none"
+          >
+            ورود / ثبت نام
+          </Link>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default HeaderProducts;
