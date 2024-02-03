776efcf30e5a033e6c7dc8dc6e4921a22338775f
sohseyedi <sohseyedi@gmail.com>
2024-02-03 20:58:18 +0330

    liked products

--- src/components/LikeAdvs.jsx
+++ src/components/LikeAdvs.jsx
@@ -1,16 +1,37 @@
-"use client"
-import * as RiIcon from 'react-icons/ri'
+"use client";
+import { likeProduct } from "@/services/productService";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { toast } from "react-hot-toast";
+import * as RiIcon from "react-icons/ri";
 
-const LikeAdvs = ({product}) => {
+const LikeAdvs = ({ product }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const likeAdvHandler = async () => {
+    try {
+      const { message } = await likeProduct(product?._id);
+      console.log(product);
+      toast.success(message);
+      router.refresh(pathname + "?" + searchParams.toString());
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
+
   return (
-    <button className="btn btn-ghost border-none text-red-600">
+    <button
+      onClick={likeAdvHandler}
+      className="btn btn-ghost border-none text-red-600"
+    >
       {product?.isLiked ? (
         <RiIcon.RiHeart3Fill className="w-6 h-6 text-red-600" />
       ) : (
         <RiIcon.RiHeart3Line className="w-6 h-6 text-red-600" />
       )}
     </button>
-  )
-}
+  );
+};
 
-export default LikeAdvs
+export default LikeAdvs;
