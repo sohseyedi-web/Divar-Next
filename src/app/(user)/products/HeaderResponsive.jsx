d7e0ac0d662c5120c33b020896cb656ae291b06e
sohseyedi <sohseyedi@gmail.com>
2024-02-05 12:12:50 +0330

    bug fixed

--- src/app/(user)/products/HeaderResponsive.jsx
+++ src/app/(user)/products/HeaderResponsive.jsx
@@ -14,29 +14,39 @@ const HeaderResponsive = () => {
 
   return (
     <footer className="lg:hidden w-full fixed bottom-0 flex items-center bg-slate-200 justify-between px-4 py-2 border-t shadow-sm">
-      <div className="flex flex-col items-center">
-        <HiIcon.HiOutlineLocationMarker size={26}/>
-        <span className="text-[1.05rem]">{user?.city}</span>
-      </div>
+      {user ? (
+        <div className="flex flex-col items-center">
+          <HiIcon.HiOutlineLocationMarker size={26} />
+          <span className="text-[1.05rem]">{user?.city}</span>
+        </div>
+      ) : null}
       <div
         className="flex flex-col items-center cursor-pointer"
         onClick={() => setActive(!active)}
       >
-        <HiIcon.HiAdjustments size={26}/>
+        <HiIcon.HiAdjustments size={26} />
         <span className="text-[1.05rem]">دسته ها</span>
       </div>
-      <Modal onClose={() => setOpen(false)} open={open} title={"ایجاد آگهی"}>
-        <AdvertisingForm onClose={() => setOpen(false)} />
-      </Modal>
-      <button
-        onClick={() => setOpen(true)}
-        className="flex flex-col items-center cursor-pointer"
-      >
-        <HiIcon.HiPlusCircle size={26}/>
-        <span className="text-[1.05rem]">ثبت آگهی</span>
-      </button>
+      {user ? (
+        <>
+          <Modal
+            onClose={() => setOpen(false)}
+            open={open}
+            title={"ایجاد آگهی"}
+          >
+            <AdvertisingForm onClose={() => setOpen(false)} />
+          </Modal>
+          <button
+            onClick={() => setOpen(true)}
+            className="flex flex-col items-center cursor-pointer"
+          >
+            <HiIcon.HiPlusCircle size={26} />
+            <span className="text-[1.05rem]">ثبت آگهی</span>
+          </button>
+        </>
+      ) : null}
       <Link href={"/profile"} className="flex flex-col items-center">
-        <HiIcon.HiUser size={26}/>
+        <HiIcon.HiUser size={26} />
         <span className="text-[1.05rem]">دیوار من</span>
       </Link>
     </footer>
